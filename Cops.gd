459ac728be38e1f01f16425be413844cd7a520cd
Wubnix <donovanwilliamsofficial@gmail.com>
2023-03-29 10:52:43 -0600

    add basic ai

--- Cops.gd
+++ Cops.gd
@@ -9,11 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var overlap = get_overlapping_areas()
-	
-	if overlap.size() > 0:
+	if get_overlapping_areas().size() > 0:
 		var player = overlap[0].get_parent()
+		var move = get_overlapping_areas()[0].get_parent().global_position
 		var health = player.get_meta("Health")
 		
-		player.get_node("UI/player_info").set_health(health + 20)
-		
-		get_parent().queue_free()
+		if get_parent().move_and_collide((move - get_parent().global_position) * delta * 2) != null:
+			get_overlapping_areas()[0].get_parent().get_node("UI/player_info").set_health(health - 10)
+
