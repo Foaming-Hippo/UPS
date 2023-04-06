e69b7c45b597cc84bef996f6ff2c092b767130db
John Crawford <coffeeboi47@protonmail.com>
2023-04-05 19:27:44 -0600

    feat: mail suckage + sound fix

--- scenes/mail.gd
+++ scenes/mail.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+var move
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +12,9 @@ func _ready():
 func _process(_delta):
 	# FIXME: this is awful
 	if get_overlapping_areas().size() > 0:
-		print("hola")
-		var move = get_overlapping_areas()[0].get_parent().get_parent().global_position
-		
-		
-		if get_parent().move_and_collide((move - get_parent().global_position) * _delta * 3) != null:
-			get_overlapping_areas()[0].get_parent().get_parent().get_node("UI/player_info").add_mail(1)
-			get_parent().queue_free()
+		player = get_overlapping_areas()[0].get_parent().get_parent()
+		move = player.global_position
+		if player.get_meta("sucking") == true:
+			if get_parent().move_and_collide((move - get_parent().global_position) * _delta * 3) != null:
+				get_overlapping_areas()[0].get_parent().get_parent().get_node("UI/player_info").add_mail(1)
+				get_parent().queue_free()
--- scenes/suckphysic.gd
+++ scenes/suckphysic.gd
@@ -1,26 +1,26 @@
 extends GPUParticlesAttractorVectorField3D
 
 var attractor_strength = -30
+var player
 var sound1
 var sound2
 
 func _ready():
-	sound1 = get_parent().get_parent().get_parent().get_node("AudioStreamPlayer3D")
-	sound2 = get_parent().get_parent().get_parent().get_node("AudioStreamPlayer3D2")
+	player = get_parent().get_parent().get_parent()
+	sound1 = player.get_node("AudioStreamPlayer3D")
+	sound2 = player.get_node("AudioStreamPlayer3D2")
 	set_process_input(true)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
+			player.set_meta("sucking", true)
 			strength = attractor_strength
-			sound1.stream = load("res://models/sounds/slurp1.tres")
+			sound1.stream = load("res://models/sounds/slurp2.tres")
 			sound1.play()
-			await sound1.finished
-			sound2.stream = load("res://models/sounds/slurp2.tres")
-			sound1.stop()
-			sound2.play()
 		else:
-			sound1.stream = load("res://models/sounds/slurp3.tres")
-			sound1.play()
-			sound2.stop()
+			player.set_meta("sucking", false)
+			sound1.stop()
+			sound2.stream = load("res://models/sounds/slurp3.tres")
+			sound2.play()
 			strength = 0.0
