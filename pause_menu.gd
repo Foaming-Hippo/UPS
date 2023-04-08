bec4d86dd23c6b3b0e97fa6944b688b8dc817076
BreadFH <70778809+Foaming-Hippo@users.noreply.github.com>
2023-04-08 14:51:30 -0600

    fixed some stuff

--- pause_menu.gd
+++ pause_menu.gd
@@ -4,6 +4,7 @@ extends ColorRect
 @onready var play_button: Button = find_child("ResumeButton")
 @onready var quit_button: Button = find_child("QuitButton")
 @onready var new_button: Button = find_child("NewButton")
+@onready var test_button: Button = find_child("TestButton")
 @onready var restart_button: Button = find_child("RestartButton")
 
 func _ready() -> void:
@@ -11,7 +12,13 @@ func _ready() -> void:
 	new_button.pressed.connect(newlevel)
 	quit_button.pressed.connect(get_tree().quit)
 	restart_button.pressed.connect(restart)
+	test_button.pressed.connect(test)
 
+func test():
+	get_tree().change_scene_to_file("res://scenes/TestLevel.tscn")
+	animator.play("Unpause")
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func newlevel():
 	get_tree().change_scene_to_file("res://level_1.tscn")
